65b4f04bb0e8e86d301e04cd66a118e59bd0bca6
Varam <prasadkurra31@gmail.com>
2025-03-11 20:29:51 -0500

    Stack using Array.

--- Exercise_1.java
+++ Exercise_1.java
@@ -7,32 +7,100 @@ class Stack {
   
     boolean isEmpty() 
     { 
-        //Write your code here 
-    } 
-
+    //     for(int i=0; i< a.length; i++){
+    // int a[] = new int[MAX]; // Maximum size of Stack 
+    //         if(a[i] == 0){
+    //             continue;
+    //         }
+    //         else
+    //             return false;
+    //     }
+    //     return true;
+        if(top == -1)
+            return true;
+        else
+            return false;
+    }
     Stack() 
     { 
-        //Initialize your constructor 
+        //Initialize your constructor
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
-        //Check for stack Overflow
-        //Write your code here
+        // if(a.length > MAX)
+        //     return false;
+        // int size=0;
+        // for(int i=0; i<a.length; i++)
+        // {
+        //     if(a[i] != 0)
+        //         size++;
+        //     else
+        //         break;
+        // }
+        // a[size+1]=x;
+        // return true;
+        if(top >=MAX -1){
+            System.out.println("Stack OverFlow");
+            return false;
+        }
+        else
+        {
+            top = top +1;
+            a[top] = x;
+        }
+        System.out.println("Pushed onto stack :"+ x);
+        return true;
     } 
   
     int pop() 
-    { 
-        //If empty return 0 and print " Stack Underflow"
-        //Write your code here
-    } 
+    {
+        // //If empty return 0 and print " Stack Underflow"
+        // if(a.length == 0)
+        //     System.out.println("Stack Underflow");
+        
+        // //Write your code here
+        // int size=0;
+        // for(int i=0; i<a.length; i++){
+        //     if(a[i] != 0)
+        //        size++;
+        //     else if(a[i] == 0)
+        //       break;
+        // }
+        // int x = a[size];
+        // a[size] =0;
+        // return x;
+        if(top == -1){
+            System.out.println("Stack Underflow.");
+            return top;
+        }
+        int x = a[top];
+        top = top -1;
+        return x;
+    }
   
     int peek() 
     { 
         //Write your code here
-    } 
-} 
-  
+    //     if(a.length > MAX)
+    //         return -1;
+    //     int size=0;
+    //     for(int i=0; i<a.length; i++)
+    //     {
+    //         if(a[i] != 0)
+    //             size++;
+    //         else
+    //             break;
+    //     }
+    //     return a[size];
+    // } 
+    if(top == -1){
+        System.out.println("Stack is empty");
+    }
+        return a[top];
+  } 
+}
 // Driver code 
 class Main { 
     public static void main(String args[]) 
@@ -42,5 +110,7 @@ class Main {
         s.push(20); 
         s.push(30); 
         System.out.println(s.pop() + " Popped from stack"); 
+        System.out.println(s.peek());
+        System.out.println(s.pop());
     } 
 }
